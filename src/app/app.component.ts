b94aa5d7eba5a3524a6a9f3165c001c5ce6282f1
faufau94 <faudelh94@hotmail.fr>
2019-01-29 23:26:00 +0100

    connexion a l'API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FilmApiService } from './film-api/film-api.service'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'film-api';
+
+
+  public film = [];
+  
+  constructor(private films: FilmApiService) {}
+
+  getFilm() {
+    this.films.getFilm("popular").subscribe(film => this.film.push(film));
+  }
+  
+  afficheFilm() {
+    return this.film;
+  }
+  ngOnInit() {
+    this.getFilm();
+    console.log(this.film);
+  }
+  
+  
 }
