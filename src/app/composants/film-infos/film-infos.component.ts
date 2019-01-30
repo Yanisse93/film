8d4e8a3b39369734a5db3509a45bc768186ae16d
faufau94 <faudelh94@hotmail.fr>
2019-01-31 00:12:52 +0100

    affichage des infos d'un film

--- src/app/composants/film-infos/film-infos.component.ts
+++ src/app/composants/film-infos/film-infos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FilmApiService } from 'src/app/services/film-api/film-api.service';
 
 @Component({
   selector: 'app-film-infos',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmInfosComponent implements OnInit {
 
-  constructor() { }
+  private filmInfo = [];
+
+  constructor(private url: ActivatedRoute, private filmService: FilmApiService) { }
 
   ngOnInit() {
+    const id = this.url.snapshot.paramMap.get('id');
+    this.filmService.getFilmInfos(id).subscribe(film => this.filmInfo.push(film));
+  }
+
+  getFilmInfos() {
+    console.log(this.filmInfo);
+    
+    return this.filmInfo;
   }
 
 }
--- src/app/services/film-api/film-api.service.ts
+++ src/app/services/film-api/film-api.service.ts
@@ -14,4 +14,8 @@ export class FilmApiService {
     const filmUrl = "https://api.themoviedb.org/3/movie/"+ choix +"?api_key=217c59632fafa28f8c88c9df26886bab&language=fr-FR";
     return this.httpClient.get<any[]>(filmUrl);
   }
+  getFilmInfos(id): Observable<any>{
+    const filmUrl = "https://api.themoviedb.org/3/movie/"+ id +"?api_key=217c59632fafa28f8c88c9df26886bab&language=fr-FR";
+    return this.httpClient.get<any>(filmUrl);
+  }
 }
